5ebd1a73d35e8cec11218c5152be0f5ad3a2642b
Oriol Ferrer Mesia <oriol@uri.cat>
2018-05-03 19:07:49 -0400

    use indices to reduce overhead

--- src/ofxDebugDrawUtils.cpp
+++ src/ofxDebugDrawUtils.cpp
@@ -11,21 +11,26 @@
 void ofxDebugDrawUtils::drawArrow(ofMesh & m, const glm::vec2 & src, const glm::vec2 & dst, float headSize,
 								  float headAngle, bool colorize, const ofColor & c){
 
+	ofIndexType firstIndex = m.getVertices().size();
+	
 	auto src3 = glm::vec3(src, 0);
 	auto dst3 = glm::vec3(dst, 0);
-	m.addVertex(src3);
-	m.addVertex(dst3);
 	auto idir = glm::normalize(src - dst);
 	float arrowHeadAng = headAngle * DEG_TO_RAD;
 	auto right = glm::vec3(glm::rotate(idir, arrowHeadAng), 0);
 	auto left = glm::vec3(glm::rotate(idir, -arrowHeadAng), 0);
-	m.addVertex(dst3);
-	m.addVertex(dst3 + right * headSize);
-	m.addVertex(dst3);
-	m.addVertex(dst3 + left * headSize);
+
+	m.addVertex(src3); 						// 0
+	m.addVertex(dst3); 						// 1
+	m.addVertex(dst3 + right * headSize); 	// 2
+	m.addVertex(dst3 + left * headSize); 	// 3
+
+	vector<ofIndexType> indices = {	firstIndex + 0, firstIndex + 1, //main line
+									firstIndex + 1, firstIndex + 2, //left arrow line
+									firstIndex + 1, firstIndex + 3
+									};
+	m.addIndices( indices );
 	if(colorize){
-		for(int i = 0; i < 6; i++){
-			m.addColor(c);
-		}
+		m.addColors({c,c,c,c});
 	}
 }
